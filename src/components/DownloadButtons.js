7e43727caa2b16cfaba97547918683556f5c1891
emenslucas <emens.lucasp@gmail.com>
2025-03-08 13:19:25 -0300

    Update DownloadButtons.js

--- src/components/DownloadButtons.js
+++ src/components/DownloadButtons.js
@@ -3,10 +3,10 @@ import React from 'react';
 const DownloadButtons = () => {
   return (
     <div id="download-container">
-      <a target="_blank" href="https://expo.dev/accounts/vinillum/projects/vinillum">
+      <a target="_blank" href="https://expo.dev/accounts/vinillum/projects/vinillum/builds/95129b43-eb01-4077-ac7b-9281fbca37bf">
       <img src="./assets/googleplay.svg" alt="Google Play" />
       </a>
-      <a target="_blank" href="https://expo.dev/accounts/vinillum/projects/vinillum">
+      <a target="_blank" href="https://expo.dev/accounts/vinillum/projects/vinillum/builds/95129b43-eb01-4077-ac7b-9281fbca37bf">
       <img src="./assets/appstore.svg" alt="App Store" />
       </a>
     </div>
